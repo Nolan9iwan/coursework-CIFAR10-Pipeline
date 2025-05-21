1c0b9bd3bdeb3437ed4f86037c4a0016d55ac187
Nolan9iwan <up2211370@myport.ac.uk>
2025-05-21 20:22:55 +0000

    examples printed with class names

--- 02_eda.py
+++ 02_eda.py
@@ -15,3 +15,23 @@ for i in range (10):
     label = y_train[i][0]
     print("Label:", label, "->", Class_names[label])
 
+#show images from set 
+plt.figure(figsize=(5, 5))
+for i in range(9):
+    plt.subplot(3, 3, i+1)
+    plt.imshow(x_train[i]) 
+    plt.title(Class_names[y_train[1][0]])
+    plt.tight_layout()
+    plt.savefig("bar_chart.png")
+
+# check how many images per class
+counts = np.bincount(y_train.flatten())
+
+# plot bar chart to show class distribution
+plt.figure(figsize=(8, 4))
+sns.barplot(x=Class_names, y=counts)
+plt.title("How many images per class (Training set)")
+plt.ylabel("Number")
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.savefig("bar_chart.png")
